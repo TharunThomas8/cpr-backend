17c9e69d8e4297304d7899da06b683d4497df0b7
Tharun <tharunthomas8@gmail.com>
2023-07-03 17:36:59 +0100

    no env

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 // config/db.js
+const url = "mongodb+srv://ajitht:OYgO3wM0Op2kACKq@cluster0.9l7kqct.mongodb.net/";
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = () => {
-  return mongoose.connect(process.env.MONGODB_URI, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
